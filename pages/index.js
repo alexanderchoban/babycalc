e86c411259034a870c179b3eaf910173993d089f
Alexander Choban <alexander.choban@gmail.com>
2021-11-06 11:28:07 -0500

    add fav icon

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,21 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Home({ dates }) {
-  const [updatedData, setUpdatedData] = useState(false)
+  const [updatedData, setUpdatedData] = useState(false);
 
   const updateData = () => {
     fetch(`/api/update`);
-    setUpdatedData(true)
+    setUpdatedData(true);
   };
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Baby Calc</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👶</text></svg>"
+        ></link>
       </Head>
 
       <nav>
